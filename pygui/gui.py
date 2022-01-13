01fd61d264173e32e65e123611d11c5857e60104
theijink <twanheijink@gmail.com>
2022-01-13 23:11:12 +0100

    updated pygui

---
+++ pygui/gui.py
@@ -0,0 +1,13 @@
+import tkinter as tk
+
+class MainWindow(tk.Tk):
+    def __init__(self):
+        super().__init__()
+        super().geometry('720x480')
+        super().title('Main Window')
+        
+
+
+
+
+
