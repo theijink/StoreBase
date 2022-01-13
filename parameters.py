5127705e99af8a3ac3391c02189fd2267a2bf68d
theijink <twanheijink@gmail.com>
2022-01-13 21:15:34 +0100

    added the parameters file

---
+++ parameters.py
@@ -0,0 +1,17 @@
+## variables
+pausetime=0.01
+date='2022-01-13'
+time='21:12:23'
+unique_filename='2022-01-13_21:12:23'
+## directories
+bindirectory=".bin"
+stickersdirectory="stickers"
+## files
+activityfilename=bindirectory+"/"+"activity.csv"
+activityfileheader=["date", "time"]
+credentialsfilename=bindirectory+"/"+"credentialsMapping.csv"
+credentialsfileheader=["code", "naam"]
+databasefilename=bindirectory+"/"+"database.csv"
+databasefileheader=["artikelnummer","benaming","kleurcode","kleur","aantal meters","aantal rollen","aantal op rol","datum","prijs per meter","totaal prijs","totaal meters","QR code"]
+stickerfilename=stickersdirectory+"/"+unique_filename
+stickerfileheader=["artikelnummer", "kleurcode", "benaming", "benaming kleur", "hoeveelheid per rol", "datum", "aantal rollen", "QR code"]
