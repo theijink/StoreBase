31a9a5dfb238563c6e74ef5da87744accd605f9e
theijink <twanheijink@gmail.com>
2022-01-14 22:21:49 +0100

    added glue code

---
+++ features/steps/f1.py
@@ -0,0 +1,13 @@
+from behave import given, when, then
+
+@given('the program is not started yet')
+def step_impl(context):
+    pass
+
+@when('the source directory is examined')
+def step_impl(context):
+    pass
+
+@then('FILENAME should be present')
+def step_impl(context):
+    pass
