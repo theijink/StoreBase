575c8d47859117bce2c1e0743bbc5600b68d8f90
theijink <twanheijink@gmail.com>
2022-01-18 22:14:13 +0100

    added test features for adding a new item to DataBasae

---
+++ features/f3.feature
@@ -0,0 +1,50 @@
+Feature: Adding a new product to the database
+
+    As user of the application
+    I should be able to add a new item to the database
+    Such that it creates a QR code, prints stickers, and the quantity is maintained
+
+    @implemented
+    Scenario Outline: Adding an item to the database
+        Given the program is "started"
+        And the "Add to DataBase" module is opened
+        When a product is entered with code <CODE>, name <NAME>, colorcode <COLORCODE>, color <COLOR>, #meters <LENGTH>, #rolls <AMOUNT>, #per_roll <ROLL_QTY>, date <DATE>, price <PRICE>, total price <PRICE_TOT>, total_length <LEN_TOT>, QR code <QR>
+        And the "add_new_item" function is executed
+        Then the code <CODE>, name <NAME>, colorcode <COLORCODE>, color <COLOR>, #meters <LENGTH>, #rolls <AMOUNT>, #per_roll <ROLL_QTY>, date <DATE>, price <PRICE>, total price <PRICE_TOT>, total_length <LEN_TOT>, QR code <QR> combination should be stored in the file <FILE>
+        Examples:
+            | CODE  | NAME  | COLORCODE | COLOR | LENGTH    | AMOUNT    | ROLL_QTY  | DATE          | PRICE | PRICE_TOT | LEN_TOT   | QR    | FILE              |
+            | T0    | test  | C0        | clr   | 1000      | 3         | 0         | 01/01/2022    | 0     | 0         | 0         | x     | databasefilename  |
+    
+    @implemented
+    Scenario Outline: Removing an item from the database
+        Given the program is "started"
+        And the "Modify DataBase" module is opened
+        And the product with QR code <QR> is stored in the file <FILE>
+        When a product is enterd by its QR code <QR>
+        And the "remove_from_database" function is executed
+        Then the product with QR code <QR> should be removed from the file <FILE>
+        Examples:
+            | QR    | FILE              |
+            | x     | databasefilename  |
+    
+    @not_implemented
+    Scenario Outline: Autocreation of QR code
+        Given the program is "started"
+    
+    @not_implemented
+    Scenario Outline: Autocreation of stickers
+        Given the program is started
+        And an item is added to the database
+
+    @not_implemented
+    Scenario Outline: Automapping of new credentials
+        Given the program is started
+        And an item is added to the database
+
+    @not_implemented
+    Scenario Outline: Autofill of mapped credentials
+        Given the program is "started"
+
+    @not_implemented
+    Scenario Outline: Autofill of date
+        Given the program is "started"
--- features/steps/f3.py
+++ features/steps/f3.py
@@ -1,2 +1,95 @@
 from behave import given, when, then
 
+def log(file, line):
+    with open('dump', 'at') as file:
+        file.write(line)
+        file.write('\n')
+
+
+@given(u'the "Add to DataBase" module is opened')
+def step_impl(context):
+    from StoreBase import db
+    context.db=db.DataBase()
+
+@given(u'the "Modify DataBase" module is opened')
+def step_impl(context):
+    from StoreBase import db
+    context.db=db.DataBase()
+
+@given(u'the product with QR code {QR} is stored in the file {FILE}')
+def step_impl(context, QR, FILE):
+    import csv
+    filename=getattr(context.db.parameters, FILE)
+    file=open(filename, 'r')
+    reader=csv.DictReader(file, delimiter=',')
+    ## check if parameter QR is listed
+    listed=[]
+    for row in reader:
+        if QR==row[context.db.parameters.databasefileheader[11]]:
+            context.item=row
+            listed.append(True)
+        else:
+            listed.append(False)
+    file.close()
+    ## passed if True occurs in list
+    assert True in listed
+
+
+@when(u'a product is entered with code {CODE}, name {NAME}, colorcode {COLORCODE}, color {COLOR}, #meters {LENGTH}, #rolls {AMOUNT}, #per_roll {ROLL_QTY}, date {DATE}, price {PRICE}, total price {PRICE_TOT}, total_length {LEN_TOT}, QR code {QR}')
+def step_impl(context, CODE, NAME, COLORCODE, COLOR, LENGTH, AMOUNT, ROLL_QTY, DATE, PRICE, PRICE_TOT, LEN_TOT, QR):
+    pass
+    fieldnames=context.db.parameters.databasefileheader
+    context.item={
+        fieldnames[0]:CODE,
+        fieldnames[1]:NAME,
+        fieldnames[2]:COLORCODE,
+        fieldnames[3]:COLOR,
+        fieldnames[4]:LENGTH,
+        fieldnames[5]:AMOUNT,
+        fieldnames[6]:ROLL_QTY,
+        fieldnames[7]:DATE,
+        fieldnames[8]:PRICE,
+        fieldnames[9]:PRICE_TOT,
+        fieldnames[10]:LEN_TOT,
+        fieldnames[11]:QR,
+    }
+
+
+@when(u'a product is enterd by its QR code {QR}')
+def step_impl(context, QR):
+    context.QR=QR
+
+@when(u'the "add_new_item" function is executed')
+def step_impl(context):
+    log('dump', str(context.item.keys()))
+    context.db.add_new_item(context.item)
+
+@when(u'the "remove_from_database" function is executed')
+def step_impl(context):
+    context.db.remove_from_database(context.item)
+
+
+@then(u'the code {CODE}, name {NAME}, colorcode {COLORCODE}, color {COLOR}, #meters {LENGTH}, #rolls {AMOUNT}, #per_roll {ROLL_QTY}, date {DATE}, price {PRICE}, total price {PRICE_TOT}, total_length {LEN_TOT}, QR code {QR} combination should be stored in the file {FILE}')
+def step_impl(context, CODE, NAME, COLORCODE, COLOR, LENGTH, AMOUNT, ROLL_QTY, DATE, PRICE, PRICE_TOT, LEN_TOT, QR, FILE):
+    import csv
+    from StoreBase import parameters
+    ## get filename and open and read file
+    filename=getattr(parameters, FILE)
+    file=open(filename, 'r')
+    reader=csv.DictReader(file, delimiter=',')
+    ## check for matching code-name
+    listed=[]
+    for row in reader:
+        if [CODE, NAME, COLORCODE, COLOR, LENGTH, AMOUNT, ROLL_QTY, DATE, PRICE, PRICE_TOT, LEN_TOT, QR] == [v for v in row.values()]:
+            listed.append(True)
+        else:
+            listed.append(False)
+    file.close()
+    ## passed if there is True in the list
+    assert True in listed
+
+
+@then(u'the product with QR code x should be removed from the file databasefilename')
+def step_impl(context):
+    pass
+
