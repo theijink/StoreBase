cbb934d4e4fd0097adbf80ed03f1a1588c539f44
theijink <twanheijink@gmail.com>
2022-01-18 18:34:00 +0100

    test feature for writing new credentials

--- features/steps/f2.py
+++ features/steps/f2.py
@@ -8,19 +8,37 @@ def log(file, line):
 
 @given(u'the credential mapping module is opened')
 def step_impl(context):
-    pass
+    from StoreBase import db
+    context.db=db.DataBase()
 
 
 @when(u'the name {NAME} and code {CODE} combination is entered')
 def step_impl(context, NAME, CODE):
-    pass
+    context.name=NAME
+    context.code=CODE
 
 
 @when(u'the "add to list" function is executed')
 def step_impl(context):
-    pass
+    context.db.add_credential(context.code, context.name)
+
 
 
 @then(u'the name {NAME} and code {CODE} combination should be stored in the file {FILE}')
 def step_impl(context, NAME, CODE, FILE):
-    pass
+    import imp
+    import csv
+    from StoreBase import parameters
+    ## get filename and open and read file
+    filename=getattr(parameters, FILE)
+    file=open(filename, 'r')
+    reader=csv.DictReader(file, delimiter=',')
+    ## check for matching code-name
+    listed=[]
+    for row in reader:
+        if [CODE, NAME] == [v for v in row.values()]:
+            listed.append(True)
+        else:
+            listed.append(False)
+    assert True in listed
+
