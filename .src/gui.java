cb5a75497c531546278c519e064a5432619bcbc0
theijink <twanheijink@gmail.com>
2022-09-03 00:34:20 +0200

    added field for logging to gui.java

--- .src/gui.java
+++ .src/gui.java
@@ -5,14 +5,20 @@ import javax.swing.JButton;
 import javax.swing.JFrame;  
 import javax.swing.JLabel;
 import javax.swing.JPanel;
-import javax.swing.JTextField;
+import javax.swing.JTextArea;
+import javax.swing.JScrollPane;
 import javax.swing.ImageIcon;
+import javax.swing.JFileChooser;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.time.ZonedDateTime;
 import java.awt.Image;
 import java.time.ZonedDateTime;
 import java.time.ZoneId;
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.InputStreamReader;
 
 public class GUI {
     public static void main(String[] args) {  
@@ -43,67 +49,112 @@ public class GUI {
         // create stickers label
         JLabel lbl1 = new JLabel("Database actions:");
         frame.add(lbl1);
-        lbl1.setBounds(50, 50, 250, 50);
+        lbl1.setBounds(50, 0, 250, 50);
                
         // create button DBadd
         JButton btn1 = new JButton("Add item to Database");
         frame.add(btn1);                    // add button to frame
         btn1.addActionListener(new LaunchDBadd());
-        btn1.setBounds(50,100, 250,50);      // (x,y,w,h) w.r.t. top left
+        btn1.setBounds(50, 50, 250,50);      // (x,y,w,h) w.r.t. top left
         
         // create button DBmod
         JButton btn2 = new JButton("Modify item in Database");
         frame.add(btn2);
         btn2.addActionListener(new LaunchDBmod());
-        btn2.setBounds(50,150, 250,50);
+        btn2.setBounds(50, 100, 250,50);
 
         // create button DBmap
         JButton btn3 = new JButton("Add code-name mapping");
         frame.add(btn3);
         btn3.addActionListener(new LaunchDBmap());
-        btn3.setBounds(50,200, 250,50);
+        btn3.setBounds(50, 150, 250,50);
 
 
         // create stickers label
         JLabel lbl2 = new JLabel("Stickers actions:");
         frame.add(lbl2);
-        lbl2.setBounds(50, 250, 250, 50);
+        lbl2.setBounds(50, 200, 250, 50);
                
         // create button DBadd
         JButton btn4 = new JButton("CCV stickers from .csv file");
         frame.add(btn4);                    // add button to frame
         btn4.setEnabled(true);
         btn4.addActionListener(new LaunchPostColumns());
-        btn4.setBounds(50,300, 250,50);      // (x,y,w,h) w.r.t. top left
+        btn4.setBounds(50, 250, 250,50);      // (x,y,w,h) w.r.t. top left
         
         // create button DBmod
         JButton btn5 = new JButton("BOL stickers from .xml file");
         frame.add(btn5);
         btn5.setEnabled(true);
         btn5.addActionListener(new LaunchSelectOrders());
-        btn5.setBounds(50,350, 250,50);
+        btn5.setBounds(50, 300, 250,50);
 
         // create button DBmap
         JButton btn6 = new JButton("PostNL stickers from .csv file");
         frame.add(btn6);
         btn6.setEnabled(false);
         btn6.addActionListener(new LaunchDBmap());
-        btn6.setBounds(50,400, 250,50);
+        btn6.setBounds(50, 350, 250,50);
 
-        frame.setLayout(null);
 
-        // create png image
+        /*// create png image
         ImageIcon img1 = new ImageIcon(new ImageIcon(".bin/statsgraph.png").getImage().getScaledInstance(270, 270, Image.SCALE_DEFAULT));
         JLabel lbl3 = new JLabel(img1);
         frame.add(lbl3);
-        lbl3.setBounds(400, 500, 270, 270);
+        lbl3.setBounds(400, 500, 270, 270);*/
 
-        /*// create text log field
-        JTextField textField = new JTextField()
-        textField.addActionListener();
-        */
+        // logging label
+        JLabel lbl3 = new JLabel("Logging:");
+        frame.add(lbl3);
+        lbl3.setBounds(350, 0, 150, 50);     
+
+        // text log field
+        JTextArea textArea = new JTextArea();
+        JScrollPane scrollPane = new JScrollPane(textArea);
+        frame.add(scrollPane);
+        //textArea.addActionListener(new FileViewer());
+        scrollPane.setBounds(350, 50, 320, 350);
+
+        // update button
+        JButton btn7 = new JButton("Update log");
+        frame.add(btn7);
+        btn7.setEnabled(true);
+        btn7.addActionListener(ev -> {
+            try {
+                BufferedReader input = new BufferedReader(new InputStreamReader(new FileInputStream(".bin/suite.log")));
+                textArea.read(input, "READING FILE");
+            } catch (Exception ex) {
+                ex.printStackTrace();
+            }
+        }); 
+        btn7.setBounds(520, 0, 150, 25);
+    
+        frame.setLayout(null);    
+
+        // start virtual environment
+        ZonedDateTime now = ZonedDateTime.now(ZoneId.systemDefault());
+        try {
+            System.out.println(now + ": starting virtual environment.");
+            //create process and execute
+            String[] cmd = { "source", ".venv/bin/activate"};
+            Process p = Runtime.getRuntime().exec(cmd);
+        } catch (Exception ex) {
+            ex.printStackTrace();
+        }
         
     }
+
+    /*// fileviewer code
+    static class FileViewer implements ActionListener{
+        public void actionPerformed (ActionEvent e){
+            try {
+                BufferedReader input = new BufferedReader(new InputStreamReader(new FileInputStream("suite.log")));
+                //textArea.read(input, "READING FILE");
+            } catch (Exception ex) {
+                ex.printStackTrace();
+            }
+        }
+    }*/
     
     // create new class for button action LaunchDBadd
     static class LaunchDBadd implements ActionListener{
